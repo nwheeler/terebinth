469af1147c8146993085bca1376f57348833b96a
Nicholas Wheeler <nwheeler@devis.com>
2013-09-25 14:35:52 -0400

    bump submodules

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ zip -r plg_content_terebinth plg_content_terebinth/
 zip -r plg_system_terebinth plg_system_terebinth/
 cd ../../
 zip -r pkg_terebinth pkg_terebinth/packages/*.zip pkg_terebinth/*.xml
+cd pkg_terebinth/packages/
+rm *.zip
+cd ../../
