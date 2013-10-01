b45b52aa789be75e60290e3382b019fbaf3e3b5b
Nicholas Wheeler <nwheeler@devis.com>
2013-10-01 18:21:09 -0400

    add support for building with plg_user_terebinth

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ cd pkg_terebinth/packages/
 zip -r com_terebinth com_terebinth/
 zip -r plg_content_terebinth plg_content_terebinth/
 zip -r plg_system_terebinth plg_system_terebinth/
+zip -r plg_user_terebinth plg_user_terebinth/
 cd ../../
 zip -r pkg_terebinth pkg_terebinth/packages/*.zip pkg_terebinth/*.xml
 cd pkg_terebinth/packages/
